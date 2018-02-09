565a0c888fff0382998dd608f421a3794655e5dc
Masha Zorin <masha.zorin@gmail.com>
2018-02-09 01:23:11 -0500

    did second part

--- anima.js
+++ anima.js
@@ -3,13 +3,15 @@ var ctx = canvas.getContext('2d')
 
 var clear = document.getElementById("clear");
 var stop = document.getElementById('stop');
+var start_circle = document.getElementById('start_circle');
+var start_dvd = document.getElementById('start_dvd');
 
 var requestId;
 
 
 var clear_canvas = function(e){
     ctx.clearRect(0,0,500,500);
-    ctx.closePath();
+    //ctx.closePath();
     stop_animation();
 }
 
@@ -17,7 +19,7 @@ var stop_animation = function() {
     window.cancelAnimationFrame(requestId);
 }
 
-var animation = function(){
+var circle_animation = function(){
     stop_animation();
     
     var radius = 0;
@@ -54,9 +56,48 @@ var animation = function(){
     draw_circle();
 }
 
-window.requestAnimationFrame(animation);
+var dvd_animation = function(){
+    stop_animation();
 
+    var rect_x = 100;
+    var rect_y = 50;
 
-canvas.addEventListener('click', animation);
+    var x = 0;
+    var y = 0;
+    var mode_x = 1; //1 if increasing, -1 if decreasing
+    var mode_y = 1;
+    
+    var draw_dvd =function(){
+	clear_canvas();
+	ctx.fillRect(x, y, rect_x, rect_y);
+	if (x > 500 - rect_x || x < 0){
+	    if (mode_x == 1){
+		mode_x = -1;
+	    }
+	    else{
+		mode_x = 1;
+	    }
+	}
+	if (y > 500 - rect_y || y < 0){
+	    if (mode_y == 1){
+		mode_y = -1;
+	    }
+	    else{
+		mode_y = 1;
+	    }
+	}
+	x += mode_x;
+	y += mode_y;
+
+	requestId = window.requestAnimationFrame(draw_dvd);
+	    
+    }
+    draw_dvd();
+}
+
+//window.requestAnimationFrame(circle_animation);
+
+start_circle.addEventListener('click', circle_animation);
+start_dvd.addEventListener('click', dvd_animation);
 clear.addEventListener('click', clear_canvas);
 stop.addEventListener('click', stop_animation);
